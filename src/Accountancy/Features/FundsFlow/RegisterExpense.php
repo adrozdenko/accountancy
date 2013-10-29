e12da48e134221009071d84cc359cd8b25a9ac89
andriy_drozdenko <adrozdenko@gmail.com>
2013-10-29 16:29:16 +0200

    Implemented expense registration scenario

--- src/Accountancy/Features/FundsFlow/RegisterExpense.php
+++ src/Accountancy/Features/FundsFlow/RegisterExpense.php
@@ -11,5 +11,28 @@ namespace Accountancy\Features\FundsFlow;
  */
 class RegisterExpense
 {
+	protected $user;
+	/**
+	* Sets the User for which income will be registered
+	* @param \Accountancy\Entity\User $user
+	*
+	* @return RegisterExpense
+	*/
+    public function setUser($user)
+    {
+        $this->user = $user;
 
+        return $this;
+    }
+
+    /**
+     * Registers expense for account
+     *
+     * @param string $accountId
+     * @param double $value
+     */
+    public function registerExpense($accountId, $value)
+    {
+        $this->user->accounts[$accountId]->debit = $value;
+    }
 }
